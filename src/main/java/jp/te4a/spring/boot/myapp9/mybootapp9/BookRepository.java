3a1516087ae1a63569b02522bdce288fb4352a7c
本田 雅空 <210008@jc-21.jp>
2024-06-14 13:00:07 +0900

    課題9登録

--- src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookRepository.java
@@ -2,7 +2,7 @@ package jp.te4a.spring.boot.myapp9.mybootapp9;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@Repository
 public interface BookRepository extends JpaRepository<BookBean, Integer>{
 
 
